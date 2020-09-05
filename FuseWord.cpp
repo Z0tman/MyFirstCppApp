d1a84814331f4d680d67c762fb889bfc919dba16
z0t_LoD <46137731+Z0tman@users.noreply.github.com>
2020-09-05 21:01:03 +0200

    Update FuseWord.cpp
    Tried to extend languages features to make standardize the user interface options.
    Only work with common lower case or upper case letter for the time being, but it is still a prototype application.

--- FuseWord.cpp
+++ FuseWord.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -5,11 +6,11 @@
 using namespace std;
 
 string FuseWord(string word_1, string word_2) {
-    //set words length
     int n = word_1.length();
     int m = word_2.length();
+    int i = 0;
+    int j = 0;
 
-    //set fusion condition as general operator list from which we will get the character to remove in the end
     string LettersToDeleteWord = "";
     for (int i_2 = 0; i_2 < n; i_2++) {
         for (int j_2 = 0; j_2 < m; j_2++) {
@@ -18,78 +19,137 @@ string FuseWord(string word_1, string word_2) {
             }
         }
     }
-    //treatment of the words before fusion.
+
     for (int k = 0; k < LettersToDeleteWord.size(); k++) {
         word_1.erase(remove(word_1.begin(), word_1.end(), LettersToDeleteWord[k]), word_1.end());
         word_2.erase(remove(word_2.begin(), word_2.end(), LettersToDeleteWord[k]), word_2.end());
     }
-    //define result variable to return after processing his value
+
     string result = "";
     result.append(word_1);
     result.append(word_2);
     return result;
 }
 
+enum Requests {
+    noRequest,
+    IntroRequest,
+    Announcer01,
+    confirm01,
+    Announcer02,
+    confirm02,
+    resultAnnounceBegin,
+    transitionResult,
+    declarationResult,
+    reverseQuestion,
+    yesReverse,
+    noReverse,
+};
+
+string DyDia(Requests quote, string UserLangEntry) {
+    if (quote == noRequest) {
+        return "";
+    }
+    if (UserLangEntry == "en") {
+        switch (quote) {
+            case(IntroRequest):
+                return "Please, choose two words to fuse together: ";
+            case(Announcer01):
+                return "First Word: ";
+            case(confirm01):
+                return " entered";
+            case(Announcer02):
+                return " Second Word: ";
+            case(confirm02):
+                return " second Word entered ready for fusion";
+            case(resultAnnounceBegin):
+                return "The fusion result of the words ";
+            case(transitionResult):
+                return " and ";
+            case(declarationResult):
+                return " is ";
+            case(reverseQuestion):
+                return "Should revert the result ?(press y or n)";
+            case(yesReverse):
+                return "The reverse of the result is: ";
+            case(noReverse):
+                return "Good Bye !";
+            default:
+                return "Invalid quote";
+        }
+    }
+    else if (UserLangEntry == "fr") {
+        switch (quote) {
+        case(IntroRequest):
+            return "Veuillez entrer deux mots que le logiciel fera fusionner: ";
+        case(Announcer01):
+            return "Premier mot: ";
+        case(confirm01):
+            return " a ete enregistre";
+        case(Announcer02):
+            return " Deuxieme mot: ";
+        case(confirm02):
+            return "a ete enregistre pour le processus de fusion.";
+        case(resultAnnounceBegin):
+            return "La fusion des mots ";
+        case(transitionResult):
+            return " et ";
+        case(declarationResult):
+            return " est ";
+        case(reverseQuestion):
+            return "Souhaitez-vous inverser le sens de lecture ?(taper o ou n)";
+        case(yesReverse):
+            return "Le resultat inverse donne : ";
+        case(noReverse):
+            return "Au revoir !";
+        default:
+            return "citation invalid";
+        }
+    }
+}
+
 int main()
 {
-    //set local variable for the application
-
     string word_1, word_2;
 
-    //set language application settings
     string UserLangEntry;
     cout << "lang option: (francais: fr - anglais: en): ";
     getline(cin, UserLangEntry);
-    if (UserLangEntry == "en") {
-        //set words request
-        cout << "Please, choose two words to fuse together: " << endl;
-        cout << "First Word: ";
+    Requests quote = noRequest;
+    DyDia(quote, UserLangEntry);
+        cout << DyDia(IntroRequest,UserLangEntry) << endl;
+        cout << DyDia(Announcer01, UserLangEntry);
         getline(cin, word_1);
-        cout << "First word " << word_1 << " entered" << endl;
-        cout << " Second Word: ";
+        cout << word_1 << DyDia(confirm01, UserLangEntry) << endl;
+        cout << DyDia(Announcer02, UserLangEntry);
         getline(cin, word_2);
-        cout << "Second Word entered ready for fusion" << endl;
-        //output the fusion result
+        cout << word_2 << DyDia(confirm02,UserLangEntry) << endl;
         string fusedWords = FuseWord(word_1, word_2);
-        cout << "The fusion result of the words " << word_1 << " and " << word_2 << " is " << fusedWords << endl;
-
-        //set user reverse option request
-        string UserEntry;
-
-        cout << "Should revert the result ?(press y or n)" << endl;
-        getline(cin, UserEntry);
-        if (UserEntry == "y" || "Y" || "yes" || "YES" || "Yes") {
-            reverse(fusedWords.begin(), fusedWords.end());
-            cout << "The reverse of the result is: " << fusedWords << endl;
-        }
-        else {
-            cout << "Good Bye !" << endl;
-        }
-    }
-    //same process with french language instructions
-    else if (UserLangEntry == "fr") {
-        cout << "Veuillez entrer deux mots que le logiciel fera fusionner: " << endl;
-        cout << "Premier mot: ";
-        getline(cin, word_1);
-        cout << "Le premier mot " << word_1 << "est enregistre pour le processus de fusion" << endl;
-        cout << " Deuxieme mot: ";
-        getline(cin, word_2);
-        cout << "Le deuxieme mot" << word_2 << " est enregitre pour le processus de fusion" << endl;
-        string fusedWords = FuseWord(word_1, word_2);
-        cout << "La fusion des mots " << word_1 << " et " << word_2 << " est " << fusedWords << endl;
+        cout << DyDia(resultAnnounceBegin, UserLangEntry) << word_1 << DyDia(transitionResult, UserLangEntry);
+        cout<< word_2 << DyDia(declarationResult,UserLangEntry) << fusedWords << endl;
 
         string UserEntry;
 
-
-        cout << "Souhaitez-vous inverser le sens de lecture ?(taper o ou n)" << endl;
+        cout << DyDia(reverseQuestion,UserLangEntry) << endl;
         getline(cin, UserEntry);
-        if (UserEntry == "o" || "O" || "oui" || "OUI" || "Oui") {
-            reverse(fusedWords.begin(), fusedWords.end());
-            cout << "Le resultat inverse donne : " << fusedWords << endl;
+        if (UserLangEntry == "en") {
+            if (UserEntry == "y" || UserEntry == "Y" | UserEntry == "Yes" || UserEntry == "yes" || UserEntry == "YES") {
+                reverse(fusedWords.begin(), fusedWords.end());
+                cout << DyDia(yesReverse, UserLangEntry) << fusedWords << endl;
+            }
+            else {
+                cout << DyDia(noReverse, UserLangEntry);
+            }
         }
-        else {
-            cout << "Au revoir !" << endl;
+        else if (UserLangEntry == "fr") {
+            if (UserEntry == "o" || "O" || "oui" || "OUI" || "Oui") {
+                reverse(fusedWords.begin(), fusedWords.end());
+                cout << DyDia(yesReverse, UserLangEntry) << fusedWords << endl;
+            }
+            else {
+                cout << DyDia(noReverse, UserLangEntry);
+            }
         }
-    }
+        
     return 0;
 }
